15b1baee5d5d463da481ce8c94d3bed780e36765
AlexFausett <139083768+AlexFausett@users.noreply.github.com>
2024-06-17 21:33:46 +1200

    Updated files that include interpolation
    This is committing my latest version of code which include both interpolation and extrapolation methods for mass flow rate, temperature and gamma for specific expansion ratios based on the data from CEA output.

--- Code in c/HybridRocketEquationsHeader.h
+++ Code in c/HybridRocketEquationsHeader.h
@@ -11,9 +11,73 @@
 #include <iterator>
 #include <vector>
 
+struct dataPoint {
+    double temperature; // oxygen fuel temperature, temperature inside chamber during reaction in Kelvin
+    double gamma;       // kappa ratio of specific heats (constant pressure vs constant volume)
+    double molecular_mass;  // mols used in reaction, molar mass (g/mol)
+    double expansionRatio;
+};
+
+// Function to perform linear extrapolation
+double linear_extrapolate(double x1, double y1, double x2, double y2, double x) {
+    return y1 + ((y2 - y1) / (x2 - x1)) * (x - x1);
+}
+
+// Function to perform linear extrapolation based on two data points
+dataPoint extrapolate_data_point(const dataPoint& dp1, const dataPoint& dp2, double expansionRatio) {
+    double temperature_extrap = linear_extrapolate(dp1.expansionRatio, dp1.temperature,
+                                                   dp2.expansionRatio, dp2.temperature, expansionRatio);
+    double gamma_extrap = linear_extrapolate(dp1.expansionRatio, dp1.gamma,
+                                             dp2.expansionRatio, dp2.gamma, expansionRatio);
+    double molecular_mass_extrap = linear_extrapolate(dp1.expansionRatio, dp1.molecular_mass,
+                                                      dp2.expansionRatio, dp2.molecular_mass, expansionRatio);
+    return {temperature_extrap, gamma_extrap, molecular_mass_extrap, expansionRatio};
+}
+
+
+// Function for linear interpolation
+double linear_interpolate(double x0, double y0, double x1, double y1, double x) {
+
+    // from https://www.cuemath.com/linear-interpolation-formula/
+    if (x0 == x1) {
+        std::cerr << "Error: x0 and x1 cannot be the same value.\n";
+        return -1;
+    }
+    return y0 + ((x - x0) * (y1 - y0) / (x1 - x0));
+}
+
+// Function to find the appropriate interval and perform interpolation to predict temperature, gamma, and molecular mass based on expansion ratio
+dataPoint interpolate_data_point(const std::vector<dataPoint>& data, double expansionRatio) {
+    // Extrapolate if values are out of range
+    if (expansionRatio <= data.front().expansionRatio) {
+        // Extrapolate based on the first two data points
+        return extrapolate_data_point(data[0], data[1], expansionRatio);
+    } else if (expansionRatio >= data.back().expansionRatio) {
+        // Extrapolate based on the last two data points
+        return extrapolate_data_point(data[data.size() - 2], data[data.size() - 1], expansionRatio);
+    } else {
+    }
+    //Interpolate if values are within range
+    for (size_t i = 0; i < data.size() - 1; ++i) {
+        if ((data[i].expansionRatio <= expansionRatio && expansionRatio <= data[i + 1].expansionRatio) ||
+            (data[i].expansionRatio >= expansionRatio && expansionRatio >= data[i + 1].expansionRatio)) {
+
+            double temperature_interp = linear_interpolate(data[i].expansionRatio, data[i].temperature,
+                                                           data[i + 1].expansionRatio, data[i + 1].temperature, expansionRatio);
+            double gamma_interp = linear_interpolate(data[i].expansionRatio, data[i].gamma,
+                                                     data[i + 1].expansionRatio, data[i + 1].gamma, expansionRatio);
+            double molecular_mass_interp = linear_interpolate(data[i].expansionRatio, data[i].molecular_mass,
+                                                              data[i + 1].expansionRatio, data[i + 1].molecular_mass, expansionRatio);
+            return {temperature_interp, gamma_interp, molecular_mass_interp, expansionRatio};
+        }
+    }
+    std::cerr << "Error: Expansion ratio value out of range for interpolation.\n";
+    return {-1, -1, -1, -1}; // Return invalid dataPoint if out of range
+}
+
 double Pe(double E, double gm1, double pr, double g, double gp1, double gm1g)
 {
-    double ff1; ///why ff1?
+    double ff1;
     ff1=1.0/E-pow((gp1/2.0),(1.0/gm1))*pow(pr,(1.0/g))*sqrt((gp1/gm1)*(1.-pow(pr,gm1g)));
     return ff1;
 }
@@ -29,7 +93,7 @@ double dPe(double E, double gm1, double pr, double g, double gp1, double gm1g)
 
 double pratio (double kappa,double E) {
     //using newton raphson and (eq 3-25) from propulsion elements
-    double gm1 = kappa - 1.0; /// why gm1 and gm1g?
+    double gm1 = kappa - 1.0;
     double gp1 = kappa + 1.0;
     double gm1g = (kappa - 1.0) / kappa;
     double g = kappa;
@@ -55,13 +119,7 @@ double pratio (double kappa,double E) {
     return pr;
 }
 
-double hybrid_rocket_thrust(double RI, double L, double T, double MOL, double kappa, double Pc, int E, double mDot_o) {
-    // # kappa is specific heat ratios
-    // # T is chamber temperature as a result of chemical reaction
-    // # e is expansion ratio
-    // # mDot_o is oxidizer flow rate
-    // # equations from Rocket Propulsion Elements unless otherwise stated
-
+double Mass_Flow_Rate(double RI, double L, double mDot_o){
     // -------------------------------Calculating Mdot propellant mass flow rate------------------------//
     // Mdot variables
     double a = 0.488;  // # regression rate coefficient of parrafin mm/s from Hybrid Rocket Fuel Regression Rate Data and Modeling
@@ -80,16 +138,46 @@ double hybrid_rocket_thrust(double RI, double L, double T, double MOL, double ka
     mDot_f = (Pf * A_b * rDot/1000);/// kg/m^3 * m^2 * m/s
     mDot = mDot_o + mDot_f;
 
-    // -------------------------------Calculating Ve Exit velocity------------------------------------//
+    return mDot;
+}
 
+double exit_velocity(double E, double Pc,double kappa, double MOL, double T){
+    // -------------------------------Calculating Ve Exit velocity------------------------------------//
+    // # equations from Rocket Propulsion Elements unless otherwise stated
     double R = 8314.41; // # Gas constant J / kmol * K
     double Pe = 0;  // # exit pressure from hybrid-rocket/Reference_code/program_2.c
     double v_e = 0; // # Calculate the exit velocity
-    double Thrust = 0; // # Calculate the rocket thrust from Space-Propulsion-Analysis-and-Design-McGraw-Hill-(1995)
 
     Pe = Pc*pratio(kappa,E);
     v_e = sqrt((2 * kappa * R * T) / ((kappa - 1)*MOL) * (1.0 - pow((Pe / Pc), ((kappa - 1) / (kappa)))));
-    Thrust = mDot * v_e;
+
+    return v_e;
+}
+
+double hybrid_rocket_thrust(const std::vector<dataPoint>& dataPoints, double gamma, double MOL, double RI, double L, double Pc, int E, double mDot_o) {
+
+    double thrust = 0; // # Calculate the rocket thrust from Space-Propulsion-Analysis-and-Design-McGraw-Hill-(1995)
+    double v_e = 0;
+
+    double mDot = Mass_Flow_Rate(RI, L, mDot_o);
+
+    // Interpolate a new data point based on expansion ratio
+    dataPoint interpData = interpolate_data_point(dataPoints, E);
+    if (interpData.temperature == -1) {
+        std::cerr << "Error: Interpolation failed. Exiting thrust calculation.\n";
+        return -1;
+    }
+
+    v_e = exit_velocity(E, Pc, interpData.gamma, interpData.molecular_mass,interpData.temperature);
+
+    // Calculate thrust using the calculated exit velocity
+    // Note: You may need to modify this calculation based on your requirements
+    // Example calculation: Thrust = mDot * v_e
+    thrust = mDot * v_e;
+
+    printf("Interpolated Data - Temperature: %f, Gamma: %f, Molecular Mass: %f, Expansion Ratio: %f, Thrust: %f\n",
+           interpData.temperature, interpData.gamma, interpData.molecular_mass, interpData.expansionRatio,thrust);
+
 
     double Ip = v_e/9.81;
 
@@ -98,6 +186,6 @@ double hybrid_rocket_thrust(double RI, double L, double T, double MOL, double ka
     printf("Specific impulse V_e: %f \n", Ip);
     */
 
-    return Thrust;
+    return thrust;
 }
 #endif //UNTITLED4_HYBRIDROCKETUPDATE9NOV_H
--- Code in c/main.cpp
+++ Code in c/main.cpp
@@ -6,83 +6,85 @@
 #include <fstream>
 #include <algorithm>
 
-int main() {
 
-    //Our rocket numbers
-    int Pc = 1;  // Chamber Pressures
+    // Create a vector of DataPoint structures to store the data
+    std::vector<dataPoint> dataPoints;
 
-    // size parameters
-    double RI = 0.2; // at time zero internal radius of fuel grain m
-    double l = 1; // our rocket
-    double oD = 0.0238166666; // maximum oxidizer flow rate kg/s
-    double volume = M_PI * RI * RI * l; // Volume of the cylindrical tank
-    double mass = volume * 900; // around 113 kg
+void ReadInData() {
+    //----------------------------------reading data---------------------------------------------------------------//
+    // Sample data with expansion ratios
+    std::string data = "1528.6   1.2983  14.856  1\n"
+                       "3017.45  1.1291  20.850  2\n"
+                       "3118.76  1.1123  23.909  3\n"
+                       "3074.36  1.1103  25.919  4\n"
+                       "3012.54  1.1104  27.371  5\n"
+                       "2947.19  1.1116  28.461  6\n"
+                       "2880.86  1.1140  29.296  7";
 
-    //Validating mass flow rate based on maximum flow rate of Geraldine
-    /*
-    1.429 kg/m^3 oxygen density
-    convert from L to m^3: 1000 L/min = 1 m^3 / min
-    1 m^3/min * 1.429 kg/m^3 = 1.429 kg/min
-    minutes to seconds = 1.429/60 =  0.0238166666 kg/s
-    */
-
-    int expansionRatio[] = {2,10,30,40,50,60,70,80,90,100};
-    // # Oxidizer Flow Rate (O2) (lbm/sec) max of testing chamber is 100 L/minute or 3.67436 lbs/s
-    double oxidizerFlowRate[] = {oD/10, oD/8,oD/6, oD/2, oD*2, oD*4, oD*8, oD*200}; /// from Geraldine test chamber at robison capable of 60000 L/s
-
-    //reading files
-    // Initialize vectors to store data
-    std::vector<double> OFtemps;  // oxygen fuel temperature, temperature inside chamber during reaction
-    std::vector<double> gammas;   // kappa
-    std::vector<double> Mol;      // mols used in reaction, molar mass
-    std::vector<double> Thrust;
-
-    // Sample data
-    std::string data = "1528.6   1.2983  14.856\n"
-                       "3017.45  1.1291  20.850\n"
-                       "3118.76  1.1123  23.909\n"
-                       "3074.36  1.1103  25.919\n"
-                       "3012.54  1.1104  27.371\n"
-                       "2947.19  1.1116  28.461\n"
-                       "2880.86  1.1140  29.296";
-
-    // Create a stringStream to read from the data string
+    // Create a stringStream to read data
     std::istringstream iss(data);
 
-    // Process each line
+    // Add each value to the vector of DataPoint structures
     std::string line;
     while (std::getline(iss, line)) {
         std::istringstream lineStream(line);
-        double temp, gamma, mol;
+        dataPoint dp;
 
-        // Read the three numbers from each line
-        lineStream >> temp >> gamma >> mol;
+        // Read the four numbers from each line
+        lineStream >> dp.temperature >> dp.gamma >> dp.molecular_mass >> dp.expansionRatio;
 
-        // Add the values to respective vectors
-        OFtemps.push_back(temp);
-        gammas.push_back(gamma);
-        Mol.push_back(mol);
+        // Add the DataPoint to the vector
+        dataPoints.push_back(dp);
     }
+}
+
+int main() {
+    //Read the data into Struct
+    ReadInData();
+
+    //Rocket Parameters
+    int Pc = 1;                                 // Chamber Pressures
+
+    // size parameters
+    double RI = 0.2;                            // at time zero internal radius of fuel grain m
+    double l = 1;                               // length
+    double oD = 0.0238166666l;                   // maximum oxidizer flow rate kg/s
+    double volume = M_PI * RI * RI * l;         // Volume of the cylindrical tank
+    double mass = volume * 900;                 // around 113 kg
+
+    int expansionRatio[] = {2,10,30,40,50,60,70,80,90,100};
+    // # Oxidizer Flow Rate (O2) (lbm/sec) max of testing chamber is 100 L/minute or 3.67436 lbs/s
+    double oxidizerFlowRate[] = {oD/10, oD/8,oD/6, oD/2, oD*2, oD*4, oD*8, oD*10}; /// from Geraldine test chamber at robison capable of 60000 L/s
+
+    // Initialize vectors to store data
+    std::vector<double> Thrust;
+    // create a thrust_data file to store results
 
     std::ofstream outFile("thrust_data.csv");
     outFile << "ExpansionRatio,OxidizerFlowRate,Thrust\n"; //csv header
 
-    for (int i =0; i < 10; i++) {
-        printf("For Expansion ratio : %i\n", expansionRatio[i]);
-        for (int j = 0; j < OFtemps.size(); j++) {
-           for (int z = 0; z < 8; z++) {
+    // Iterate over data points
+    for (const auto& data : dataPoints) {
+        // iterate through expansion ratio, vectored data, and oxidizer flow rates
+        for (int i = 0; i < 10; i++) {
+            //printf("For Expansion ratio : %i\n", expansionRatio[i]);
+            for (int z = 0; z < 8; z++) {
 
-               double thrust = hybrid_rocket_thrust(RI, l, OFtemps[j], Mol[j], gammas[j], Pc, expansionRatio[i],oxidizerFlowRate[z]);
+                double thrust = hybrid_rocket_thrust(dataPoints, data.gamma,data.molecular_mass,RI, l, Pc, expansionRatio[i], oxidizerFlowRate[z]);
+                //double thrust = hybrid_rocket_thrust(dataPoints, data.gamma,data.molecular_mass,RI, l, Pc, data.expansionRatio, oxidizerFlowRate[z]);
 
                 //printf("mol %f",Mol[j]);
                 //printf("temp %f",OFtemps[j]);
-                printf("Thrust : %f \n", thrust);
-               Thrust.push_back(thrust);
-               outFile << expansionRatio[i] << "," << oxidizerFlowRate[z] << "," << thrust << "\n"; // adds data to csv file
+                //printf("Thrust : %f \n", thrust);
+                Thrust.push_back(thrust);
+
+                outFile << expansionRatio[i] << "," << oxidizerFlowRate[z] << "," << thrust
+                        << "\n"; // adds data to csv file
             }
         }
     }
-    // returns largest value from the values
+
+    // returns largest value from calculated Thrust for each expansion ratio
     auto max_element_iterator = std::max_element(Thrust.begin(), Thrust.end());
     if (max_element_iterator != Thrust.end()) {
         double largest_element = *max_element_iterator;
@@ -91,5 +93,12 @@ int main() {
         std::cout << "Vector is empty." << std::endl;
     }
 
+    //Validating mass flow rate based on maximum flow rate of Geraldine
+    /*
+    1.429 kg/m^3 oxygen density
+    convert from L to m^3: 1000 L/min = 1 m^3 / min
+    1 m^3/min * 1.429 kg/m^3 = 1.429 kg/min
+    minutes to seconds = 1.429/60 =  0.0238166666 kg/s
+    */
 }
 
