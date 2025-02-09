33789097f3311ebc4d4c2e080f9ca049d1497d8f
AlexFausett <139083768+AlexFausett@users.noreply.github.com>
2025-02-10 11:27:59 +1300

    Tidied code
    Tidied code for better readability and added header file to increase readability and code flow. Also removed some redundancy in code. It should be noted the code requires the header file to run now.

--- Code in c/WhatItTakesCode/WhatItTakes.cpp
+++ Code in c/WhatItTakesCode/WhatItTakes.cpp
@@ -3,92 +3,39 @@
 #include <fstream>
 #include <array>
 #include <functional>
-// Structure to hold angle, CL, and CD values
-struct CoeffData {
-    double angle;
-    double CL;
-    double CD;
-};
+#include "WhatItTakes.h"
 
-struct Rocket {
-    double m0;        // Initial mass
-    double mprop;     // Propellant mass
-    double mpl;       // Payload mass
-    double mstruc;    // Structural mass
-    double tburn;     // Burn time
-    double thrust;    // Thrust force
-    double mDot;      // Mass flow rate
-    double diam;      // Diameter
-    double A;         // Area (cross-sectional)
-    double L;         // Length of rocket
-};
+/// Equations and other methods are stored in WhatItTakes.h
 
-
-// Function to load data from CSV file
-std::vector<CoeffData> loadCoeffData(const std::string& filename) {
-    std::vector<CoeffData> data;
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: Cannot open file " << filename << std::endl;
-        return data;
-    }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        CoeffData dp;
-        if (sscanf(line.c_str(), "%lf,%lf,%lf", &dp.angle, &dp.CL, &dp.CD) == 3) {
-            data.push_back(dp);
-        }
-    }
-
-    file.close();
-    return data;
-}
-
-void getCL_CD(double angle, std::vector<CoeffData>& data, double& CL, double& CD){
-   // Interpolate the value of CL and CD based on a given angle CD and CL from data
-   for(int i = 0; i < data.size()-1; i++){
-       if(angle >= data[i].angle && angle < data[i+1].angle ) {
-           double slopeCL = ((data[i+1].CL-data[i].CL)/(data[i+1].angle - data[i].angle));
-           double slopeCD = ((data[i+1].CD-data[i].CD)/(data[i+1].angle - data[i].angle));
-           CL = data[i].CL + (slopeCL) * (angle - data[i].angle);
-           CD = data[i].CD + (slopeCD) * (angle - data[i].angle);
-           return;
-       }
-   }
-   //sets the values to zero if out of range
-   CL = 0.0;
-   CD = 0.0;
-}
-
-// Function to convert degrees to radians
-double deg2rad(double degrees) {
-    return degrees * M_PI / 180.0;
-}
-
-// Function to convert radians to degrees
-double rad2deg(double radians) {
-    return radians * 180.0 / M_PI;
-}
+//function prototypes
+void rk4(double& v, double& h, double& psi, double& theta, double& phi, double& t, double dt, double m, double T, double Re, double g0, double hscale, double rho0, double A, double CD, double CL,double AL,double wx, double wy, double wz);
 
 // Function to calculate the derivatives of the system
-std::array<double, 4> derivatives(double t, double v, double h, double psi, double m, double T, double Re, double g0, double hscale, double rho0, double A, double CD, double CL, double AL) {
-    double g = g0 / pow((1 + h / Re), 2);
-    double rho = rho0 * exp(-h / hscale);
-    double drag = (1 / 2) * CD * rho * pow(v, 2) * A;
-    double lift = (1 / 2) * CL * rho * pow(v, 2) * AL;
+std::array<double, 10> derivatives(double t, double v, double h, double psi, double theta, double phi, double m, double T, double Re, double g0, double hscale, double rho0, double A, double CD, double CL, double AL, double wx,double wy, double wz) {
+    double g = g0 / pow((1 + h / Re), 2);    // Gravity at height
+    double rho = rho0 * exp(-h / hscale);       // air density constant
+    double drag = 0.5 * CD * rho * pow(v, 2) * A;
+    double lift = 0.5 * CL * rho * pow(v, 2) * AL;
     double Dv_dt, h_dot, psi_dot, theta_dot;
+    double dphi, dtheta, dpsi; // For Euler Angles
 
     if (h <= 15000 && T>0) { // before gravity turn currently at karman line possibly to high??
         Dv_dt = T / m - drag / m - g + lift/m;
         h_dot = v;
+
+        // constant thrust results in no angle changes
         psi_dot = 0;
         theta_dot = 0;
+        dphi = 0;
+        dtheta = 0;
+        dpsi = 0;
 
     } else { // after gravity turn
         // look at doing it differently here
 
         Dv_dt = T / m + drag / m + lift/m - g * cos(psi);
+
+        // No thrust results in angle changes
         double phi_dot = g * sin(psi) / v; // defined here due to scope
         h_dot = v * cos(psi);
         theta_dot = (v * sin(psi)) / (Re + h);
@@ -97,65 +44,32 @@ std::array<double, 4> derivatives(double t, double v, double h, double psi, doub
         //double vx = v * cos(psi);
         //double vy = v * sin(psi);
         //psi_dot = psi + atan2(vx,vy);
-        //psuedo code
-        //  use trig here to solve psi dot then let rk4 approximate the true value?
+
+        // Get the torque based on thrust, lift, and drag
+        auto torque = calculateTorque(lift, drag, radius);
+
+        Inertia inertia = calculateInertia(diam, L, m);
+
+        // Update angular velocities
+        std::array<double, 3> dw_dot = calculateAngularAccelerations(inertia.Ix,inertia.Iy,inertia.Iz, wx, wy, wz, torque[0], torque[1], torque[2]);
+
+        // Integrate the angular velocity to angular displacement
+        dphi = dw_dot[0]*t;
+        dtheta = dw_dot[1]*t;
+        dpsi = dw_dot[2]*t;
+
+        // Compute the time derivatives of the Euler angles based on angular velocities
+        //dphi = wx + sin(phi) * tan(theta) * wy + cos(phi) * tan(theta) * wz;
+        //dtheta = cos(phi) * wy - sin(phi) * wz;
+        //dpsi = (sin(phi) / cos(theta)) * wy + (cos(phi) / cos(theta)) * wz;
+
+        // psi_dot = dpsi;
     }
-    return {Dv_dt, h_dot, psi_dot, theta_dot};
-}
-
-// Function to perform the RK4 method
-void rk4(double& v, double& h, double& psi, double& t, double dt, double m, double T, double Re, double g0, double hscale, double rho0, double A, double CD, double CL,double AL) {
-    // K1 beginning of the interval using Eulers method
-    // K2 the midpoint of the interval
-    // K3 again midpoint of the interval
-    // K4 end of the interval
-    // sum weighted v, h, psi, t
-
-    auto k1 = derivatives(t, v, h, psi, m, T, Re, g0, hscale, rho0, A, CD, CL, AL);
-    auto k2 = derivatives(t + dt / 2, v + k1[0] * dt / 2, h + k1[1] * dt / 2, psi + k1[2] * dt / 2, m, T, Re, g0, hscale, rho0, A, CD, CL, AL);
-    auto k3 = derivatives(t + dt / 2, v + k2[0] * dt / 2, h + k2[1] * dt / 2, psi + k2[2] * dt / 2, m, T, Re, g0, hscale, rho0, A, CD, CL, AL);
-    auto k4 = derivatives(t + dt, v + k3[0] * dt, h + k3[1] * dt, psi + k3[2] * dt, m, T, Re, g0, hscale, rho0, A, CD, CL, AL);
-
-    //middle points weighted by 1/3 and ends weighted by 1/6 to achieve 4th order accuracy
-    v += (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) * dt / 6.0; // yn + 1
-    h += (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) * dt / 6.0;
-    psi += (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]) * dt / 6.0;
-    t += dt;
-}
-
-// Function to calculate pitching moment coefficient (C_M)
-double calculateCM(double alpha){
-    // Example formula replace this with empirical data or look-up tables
-    return 0.1 * alpha; // A simple linear relationship for small angles
-}
-
-// Function to calculate the pitching moment
-double calculatePitchingMoment(double velocity, double alpha, double rho, double diam, double L) {
-    double S = M_PI*pow((diam/2),2); // reference area
-    double CM = calculateCM(alpha); // moment co-effecient
-    double dynamicPressure = 0.5 * rho * std::pow(velocity, 2);
-    double M = CM * dynamicPressure * S * L;  // calculating the moment
-    return M;
+    return {Dv_dt, h_dot, psi_dot, theta_dot,dphi,dtheta,dpsi};
 }
 
 int main() {
-    // Based on the Titan 2 and Gemini Spacecraft
-    // Rocket parameters stored in a struct
-    Rocket titan2 {
-            .m0 = 111130 + 32000 + 6736,   // Initial mass = propellant + payload + structure
-            .mprop = 111130.0,                   // kg propellent mass
-            .mpl = 32000.0,                      // kg payload mass
-            .mstruc = 6736.0,                    // kg structure mass
-            .tburn = 356.0,                      // burn time in seconds
-            .thrust = 1900000.0,                 // Newtons
-            .mDot = titan2.mprop / titan2.tburn, // kg/s propellent mass flow rate
-            .diam = 3.05,                        // rocket diameter meters
-            .A = M_PI * pow((3.05 / 2), 2),      // m^2 (cross-sectional area or area of nose cone)
-            .L = 33.0                            // meters (rocket length)
-    };
-
-    double m = 0.0; // resetting active mass to 0
-    double T = titan2.thrust;
+    double T = thrust;
 
     // Atmospheric Conditions
     double g0 = 9.81; // gravity at sea level
@@ -185,35 +99,39 @@ int main() {
         return 1;
     }
 
-    // RK4 method loop
+    // Simulation begins, RK4 method loop
     while (t <= t_end) {
+
         // calculate Lift co-efficient
         getCL_CD(rad2deg(psi), data, CL, CD);
-        //printf("%f",CL);
 
         // Update rocket mass and thrust
-        m = (t <= titan2.tburn) ? (titan2.m0 - titan2.mDot * t) : titan2.mstruc;
-        T = (t <= titan2.tburn) ? titan2.thrust : 0.0;
-
-        // Perform RK4 integration step
-        rk4(v, h, psi, t, dt, m, T, Re, g0, hscale, rho0, titan2.A, CD, CL, titan2.A);
+        double m = (t <= tburn) ? (m0 - mDot * t) : mstruc;
+        T = (t <= tburn) ? thrust : 0.0;
 
         // Components of velocity
         double vx = v *cos(psi);
         double vy = v *sin(psi);
         double psi2 = atan2(vy, vx);
         //printf("%f %n",   rad2deg(psi2));
-
         double vtot = sqrt(pow(vx,2) + pow(vy,2));
 
         // Calculate distance traveled along the Earth's surface
         double theta = (v * sin(psi)) / (Re + h) * dt;
         double dr = theta * Re / 1000;
 
-        // Calculate pitching moment
-        //double pitchingMoment = calculatePitchingMoment(v, theta, rho0, titan2.diam, titan2.L);
-        double pitchingMoment = 0;
+        double phi; // idk how to acturally get this yet
 
+        // Calculate pitching moment
+        double pitchingMoment = calculatePitchingMoment(v, theta, rho0, diam, L);
+        //double pitchingMoment = 0;
+
+        // attempt to rectify code
+        double wx = 1;
+        double wy = 1;
+        double wz = 1;
+        // Perform RK4 integration step
+        rk4(v, h, psi, theta, phi, t, dt, m, T, Re, g0, hscale, rho0, A, CD, CL, A,wx,wy,wz);
 
         // Write current time, velocity, and position to file
         outfile << t << "," << v << "," << dr << "," << h << "," << rad2deg(psi) << "," << vy << "," << vx << "," << rad2deg(psi2) << "\n";
@@ -233,3 +151,35 @@ int main() {
 
     return 0;
 }
+
+// Function to perform the RK4 method Runge Kutta
+void rk4(double& v, double& h, double& psi, double& theta, double& phi, double& t, double dt, double m, double T, double Re, double g0, double hscale, double rho0, double A, double CD, double CL,double AL,double wx, double wy, double wz) {
+    // K1 beginning of the interval using Euler's method
+    // K2 the midpoint of the interval
+    // K3 again midpoint of the interval
+    // K4 end of the interval
+    // sum weighted v, h, psi, t
+
+    auto k1 = derivatives(t, v, h, psi, theta, phi, m, T, Re, g0, hscale, rho0, A, CD, CL, AL, wx, wy, wz);
+    auto k2 = derivatives(t + dt / 2, v + k1[0] * dt / 2, h + k1[1] * dt / 2, psi + k1[2] * dt / 2,theta + k1[3] * dt / 2, phi + k1[4] * dt / 2, m, T, Re, g0, hscale, rho0, A, CD, CL, AL, wx, wy, wz);
+    auto k3 = derivatives(t + dt / 2, v + k2[0] * dt / 2, h + k2[1] * dt / 2, psi + k2[2] * dt / 2,theta + k2[3] * dt / 2, phi + k2[4] * dt / 2, m, T, Re, g0, hscale, rho0, A, CD, CL, AL, wx, wy, wz);
+    auto k4 = derivatives(t + dt, v + k3[0] * dt, h + k3[1] * dt, psi + k3[2] * dt, theta + k3[3] * dt,phi + k3[4] * dt, m, T, Re, g0, hscale, rho0, A, CD, CL, AL, wx, wy, wz);
+
+    //middle points weighted by 1/3 and ends weighted by 1/6 to achieve 4th order accuracy
+    v += (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) * dt / 6.0; // yn + 1
+    h += (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) * dt / 6.0;
+    psi += (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]) * dt / 6.0;
+    theta += (k1[3] + 2 * k2[3] + 2 * k3[3] + k4[3]) * dt / 6.0;
+    phi += (k1[4] + 2 * k2[4] + 2 * k3[4] + k4[4]) * dt / 6.0;
+    t += dt;
+
+    // Update angular position (Euler angles)
+    double phi_increment = (k1[4] + 2 * k2[4] + 2 * k3[4] + k4[4]) * dt / 6.0;
+    double theta_increment = (k1[5] + 2 * k2[5] + 2 * k3[5] + k4[5]) * dt / 6.0;
+    double psi_increment = (k1[6] + 2 * k2[6] + 2 * k3[6] + k4[6]) * dt / 6.0;
+
+    phi += phi_increment;
+    theta += theta_increment;
+    psi += psi_increment;
+}
+
