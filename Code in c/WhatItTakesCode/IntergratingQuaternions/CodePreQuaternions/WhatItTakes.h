32379e060db4cfd0e8b8c051043fe9f29c23b8cf
AlexFausett <139083768+AlexFausett@users.noreply.github.com>
2025-02-10 12:00:09 +1300

    Reduced Variables
    made some variables global to reduce the number of constants being called in functions.

--- Code in c/WhatItTakesCode/IntergratingQuaternions/CodePreQuaternions/WhatItTakes.h
+++ Code in c/WhatItTakesCode/IntergratingQuaternions/CodePreQuaternions/WhatItTakes.h
@@ -129,5 +129,73 @@ double calculatePitchingMoment(double velocity, double alpha, double rho, double
     return M;
 }
 
+//----------------------------------------------------------- Quaternion related code -----------------------------------------------------------PITCHING
+
+typedef struct { double w, x, y, z; } Quaternion;
+typedef struct { double x, y, z; } Vector3;
+
+// Quaternion functions (multiply, conjugate, rotate vector, etc.)
+Quaternion quaternion_multiply(Quaternion q1, Quaternion q2) {
+    Quaternion result;
+    result.w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
+    result.x = q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y;
+    result.y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
+    result.z = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
+    return result;
+}
+
+Quaternion quaternion_conjugate(Quaternion q) {
+    Quaternion result = {q.w, -q.x, -q.y, -q.z};
+    return result;
+}
+
+// Update quaternion using angular velocities
+Vector3 rotate_vector(Quaternion q, double vx, double vy, double vz) {
+    Quaternion v = {0, vx, vy, vz};
+    Quaternion q_conj = quaternion_conjugate(q);
+    Quaternion rotated_q = quaternion_multiply(quaternion_multiply(q, v), q_conj);
+
+    return {rotated_q.x, rotated_q.y, rotated_q.z};
+}
+
+Quaternion quaternion_scale(Quaternion q, double scale) {
+    Quaternion result;
+    result.w = q.w * scale;
+    result.x = q.x * scale;
+    result.y = q.y * scale;
+    result.z = q.z * scale;
+    return result;
+}
+
+Quaternion quaternion_add(Quaternion q1, Quaternion q2) {
+    Quaternion result;
+    result.w = q1.w + q2.w;
+    result.x = q1.x + q2.x;
+    result.y = q1.y + q2.y;
+    result.z = q1.z + q2.z;
+    return result;
+}
+
+// Normalize quaternion to avoid drift
+Quaternion quaternion_normalize(Quaternion q) {
+    double norm = sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
+    return {q.w / norm, q.x / norm, q.y / norm, q.z / norm};
+}
+
+// Runge-Kutta 4 method for quaternion update
+Quaternion rk4_quaternion_update(Quaternion q, Quaternion omega_q, double dt) {
+    Quaternion k1 = quaternion_scale(quaternion_multiply(q, omega_q), 0.5 * dt);
+    Quaternion k2 = quaternion_scale(quaternion_multiply(quaternion_add(q, k1), omega_q), 0.5 * dt);
+    Quaternion k3 = quaternion_scale(quaternion_multiply(quaternion_add(q, k2), omega_q), dt);
+    Quaternion k4 = quaternion_scale(quaternion_multiply(quaternion_add(q, k3), omega_q), dt);
+
+    Quaternion q_final = quaternion_add(q, quaternion_scale(k1, 1.0 / 6.0));
+    q_final = quaternion_add(q_final, quaternion_scale(k2, 2.0 / 6.0));
+    q_final = quaternion_add(q_final, quaternion_scale(k3, 2.0 / 6.0));
+    q_final = quaternion_add(q_final, quaternion_scale(k4, 1.0 / 6.0));
+
+    return quaternion_normalize(q_final);
+}
+
 
 #endif //WHATITTAKES_WHATITTAKES_H
